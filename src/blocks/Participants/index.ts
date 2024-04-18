fd94bd72cbd3ff9cc2ed404f6fdcfc3cc3510275
ignaskmn <ignas@kmn.lt>
2024-04-18 15:04:21 +0300

    Update program and participants blocks, add Contacts block, and update Pages collection configuration

--- src/blocks/Participants/index.ts
+++ src/blocks/Participants/index.ts
@@ -1,4 +1,16 @@
 import { Block } from "payload/types";
+import {
+  lexicalRichTextField,
+  EmojisFeature,
+  EmojiPickerFeature,
+  HorizontalRuleFeature,
+  YouTubeFeature,
+  TwitterFeature,
+  ClearEditorFeature,
+  ReadOnlyModeFeature,
+  KeywordsFeature,
+  LinkFeature,
+} from "payload-plugin-lexical";
 
 export const Participants: Block = {
   slug: "participantsBlock",
@@ -30,16 +42,33 @@ export const Participants: Block = {
           type: "text",
           localized: true,
         },
-        {
-          name: "bio",
-          type: "richText",
-          admin: {
-            elements: ["link", "ol", "ul"],
-            leaves: ["bold", "italic", "underline"],
-          },
+        lexicalRichTextField({
+          name: "content",
           localized: true,
-          required: true,
-        },
+          label: "Text Content",
+          editorConfigModifier: (defaultEditorConfig) => {
+            defaultEditorConfig.debug = false;
+            defaultEditorConfig.toggles.textColor.enabled = false;
+            defaultEditorConfig.toggles.textBackground.enabled = false;
+            defaultEditorConfig.toggles.fontSize.enabled = false;
+            defaultEditorConfig.toggles.font.enabled = false;
+            defaultEditorConfig.toggles.align.enabled = false;
+
+            defaultEditorConfig.features = [
+              EmojisFeature({}), // Adds new Emoji nodes with new, different-looking emojis
+              EmojiPickerFeature({}), // Use in combination with EmojisPlugin. When you start typing ":" it will show you different emojis you can use. They also look different!
+              HorizontalRuleFeature({}), // Horizontal rule in the editor.
+              YouTubeFeature({}), // YouTube Embed
+              TwitterFeature({}), // Twitter Embed
+              ClearEditorFeature({}), // Adds a button in the action menu which clears the editor
+              ReadOnlyModeFeature({}), // Acion button: toggle read-only mode on or off
+              KeywordsFeature({}), // Highlights certain words
+              LinkFeature({}), // Obvious: hyperlinks! This includes the AutoLink plugin.
+            ];
+
+            return defaultEditorConfig;
+          },
+        }),
       ],
     },
   ],
